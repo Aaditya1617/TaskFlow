77102f8d47023895e6c699d87006f899ecfa5813
Aaditya <aadityadahiya1617@gmail.com>
2025-07-25 12:46:57 +0530

    Remove

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://aadityasingh1617:resumebuilder1617@cluster0.jxytyht.mongodb.net/TaskManager')
-        .then(() => console.log("DB CONNECTED"));
-}
-
