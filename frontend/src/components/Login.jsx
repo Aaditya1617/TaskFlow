0005207448befb960f54f2d8b1e6583be097067f
Aaditya <aadityadahiya1617@gmail.com>
2025-07-25 13:09:41 +0530

    pushing backend url changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
   const [rememberMe, setRememberMe] = useState(false)
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const url = "http://localhost:4000"
+  const url = "https://taskflow-backend-b1yb.onrender.com/"
 
   // Auto-login
   useEffect(() => {
